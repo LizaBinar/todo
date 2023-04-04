98245c2da7db1ed855c89960be8c93bdc5860fca
LizaBinar <106739245+LizaBinar@users.noreply.github.com>
2023-04-04 16:20:22 +0700

    init commit

--- src/components/task-list/task-list.js
+++ src/components/task-list/task-list.js
@@ -1,6 +1,6 @@
 import './task-list.css';
 import PropTypes from 'prop-types';
-import { Component } from 'react';
+import {Component} from 'react';
 
 import Task from '../task/task';
 
@@ -11,42 +11,43 @@ function throwError(label) {
 
 
 
-  throw new Error(`Ты не передал ${label}`);
+
+    throw new Error(`Ты не передал ${label}`);
 }
 
 class TaskList extends Component {
-  static propTypes = {
-    todoData: PropTypes.arrayOf(PropTypes.object),
-    onDeleted: PropTypes.func,
-    onCompleted: PropTypes.func,
-  };
+    static propTypes = {
+        todoData: PropTypes.arrayOf(PropTypes.object),
+        onDeleted: PropTypes.func,
+        onCompleted: PropTypes.func,
+    };
 
-  static defaultProps = {
-    todoData: [
-      {
-        label: 'default',
-        completed: false,
-        id: 3,
-        dateCreated: Date.now(),
-      },
-    ],
-    onDeleted: throwError.bind(this, 'onDeleted'),
-    onCompleted: throwError.bind(this, 'onCompleted'),
-  };
+    static defaultProps = {
+        todoData: [
+            {
+                label: 'default',
+                completed: false,
+                id: 3,
+                dateCreated: Date.now(),
+            },
+        ],
+        onDeleted: throwError.bind(this, 'onDeleted'),
+        onCompleted: throwError.bind(this, 'onCompleted'),
+    };
 
-  render() {
-    const elements = this.props.todoData.map((item) => (
-      <li key={item.id}>
-        <Task
-          item={item}
-          onDeleted={() => this.props.onDeleted(item.id)}
-          onCompleted={() => this.props.onCompleted(item.id)}
-        />
-      </li>
-    ));
+    render() {
+        const elements = this.props.todoData.map((item) => (
+            <li key={item.id}>
+                <Task
+                    item={item}
+                    onDeleted={() => this.props.onDeleted(item.id)}
+                    onCompleted={() => this.props.onCompleted(item.id)}
+                />
+            </li>
+        ));
 
-    return <ul className="todo-list">{elements}</ul>;
-  }
+        return <ul className="todo-list">{elements}</ul>;
+    }
 }
 
 export default TaskList;
